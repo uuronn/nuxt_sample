333aa5034203ec72a82bf07d7fb01278f2ef448b
uuronn <uuronprogram@gmail.com>
2021-08-22 23:14:11 +0900

    getIp_feature

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/element.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -33,6 +34,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
---
+++ pages/element.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <el-button @click="visible = true">ダイアログボタンを開く</el-button>
+    <el-dialog :visible.sync="visible" title="確認">
+      <p>処理されました</p>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visible: false
+    }
+  }
+}
+</script>
