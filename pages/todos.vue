8fd3e7b834021e372a72a4bc8b9a252315a8efbc
uuronn <uuronprogram@gmail.com>
2021-08-23 13:41:10 +0900

    nuxt

---
+++ pages/todos.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="todo in todos" :key="todo">
+        <input type="checkbox" @change="toggle(todo)">
+        <span>{{ todo.text }}</span>
+      </li>
+    </ul>
+    <input placeholder="タスクを追加" @keyup.enter="addTodo">
+  </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+  methods: {
+    addTodo(e) {
+      this.$store.commit('todos/add', e.target.value)
+    },
+    ...mapMutations({
+      toggle: 'todos/toggle'
+    })
+  },
+  computed: {
+    todos() {
+      return this.$store.state.todos.list
+    }
+  }
+}
+</script>
